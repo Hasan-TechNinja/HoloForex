1b451e2777533192fe717c47be9ac6b5fb02394f
Hasan-TechNinja <hasantechninaj@gmail.com>
2025-05-10 06:02:45 +0600

    category blog & comment model created

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
-from . models import About_Us
+from . models import About, Blog, Category, Comment
 
 # Register your models here.
 
-class AboutUS(admin.ModelAdmin):
+class AboutModelAdmin(admin.ModelAdmin):
     list_display = (
-
+        'id', 'user', 'name', 'about', 'about_image', 'mission', 'mission_image', 'vision', 'we_offer', 'why_choose', 'choose_image'
     )
-admin.site.register(About_Us)
+admin.site.register(About, AboutModelAdmin)
+
+class BlogModelAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user', 'title', 'image', 'category', 'description', 'date', 'view'
+    )
+admin.site.register(Blog, BlogModelAdmin)
+
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user', 'name', 'description', 'image'
+    )
+admin.site.register(Category, CategoryModelAdmin)
+
+
+class CommentModelAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user', 'blog', 'comment'
+    )
+admin.site.register(Comment, CommentModelAdmin)
--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from tinymce.models import HTMLField
+from django.contrib.auth.models import User
 
 # Create your models here.
-class About_Us(models.Model):
+class About(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     about = models.CharField(max_length=800)
     about_image = models.ImageField(upload_to='about')
@@ -12,4 +14,36 @@ class About_Us(models.Model):
     we_offer = models.CharField(max_length=500)
     why_choose = models.TextField()
     choose_image = models.ImageField(upload_to='WhyChoose')
+
+    def __str__(self):
+        return self.name
     
+
+class Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="category", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Blog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='Blog', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
+    description = HTMLField()
+    date = models.DateTimeField(auto_now=True)
+    view = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='blogs')
+    comment = models.TextField(max_length=800)
+
+    def __str__(self):
+        return f"{self.blog.title}s Review"
